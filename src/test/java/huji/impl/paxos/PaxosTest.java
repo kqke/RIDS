81642846b1aaf35c70f031d9b83a15bd400f00c0
itaialon <itaialon@mail.huji.ac.il>
2020-07-09 15:27:54 +0300

    Version 2.0
    split the system between logics

--- src/test/java/huji/impl/paxos/PaxosTest.java
+++ src/test/java/huji/impl/paxos/PaxosTest.java
@@ -1,7 +1,13 @@
 package huji.impl.paxos;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaxosTest {
 
+    @Test
+    public void run() {
+
+    }
 }
